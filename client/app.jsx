a26b9b7b965f83b6e37540c62dcf9fced80376d3
Udaibir Virk <udaibirv@gmail.com>
2021-11-05 18:41:23 +0000

    working through useEffect hook

--- client/app.jsx
+++ client/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './pages/home';
 
 const App = props => {
@@ -11,6 +11,21 @@ const App = props => {
     setState({ england: info });
   };
 
+  useEffect(() => {
+    fetch('/api/leauge-info/england')
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        const table = result.response[0].league.standings;
+        table.map((club, index) => {
+          return (
+            setState({ england: club, season: '16/17' })
+          );
+        });
+
+      });
+  });
   const content = (
 
     <h1>Hello</h1>
