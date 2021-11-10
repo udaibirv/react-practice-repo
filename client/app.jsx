b58712b5a720287e4d0ee0ca40d677d8e5ae8909
udaibirv <udaibirv@gmail.com>
2021-11-09 16:07:37 -0800

    added multiple calls to API to recieve info from different seasons.

--- client/app.jsx
+++ client/app.jsx
@@ -4,13 +4,12 @@ import Home from './pages/home';
 const App = () => {
   const [club, setTeam] = useState([]);
 
-  useEffect(() => {
+  const fetchData2018 = () => {
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
-
-    fetch('/api/leauge-info/england', requestOptions)
+    fetch('/api/leauge-info/england/2018', requestOptions)
       .then(response => response.json())
       .then(data => {
         const table = data.response;
@@ -18,11 +17,31 @@ const App = () => {
         setTeam(leagueTable);
       });
 
+  };
+
+  const fetchData2020 = () => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch('/api/leauge-info/england', requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        const table = data.response;
+        const leagueTable = data.response[0].league.standings[0];
+        setTeam(leagueTable);
+      });
+  };
+
+  useEffect(() => {
+    fetchData2020();
+
   }, [setTeam]);
 
   return (
 
     <div>
+      <button className="england-button btn btn-sm" onClick={fetchData2018}>18/19</button>
       {
         club.map((teamName, key) => {
           return (
--- server/index.js
+++ server/index.js
@@ -30,6 +30,26 @@ app.get('/api/leauge-info/england', (req, res) => {
 
 });
 
+app.get('/api/leauge-info/england/2018', (req, res) => {
+  request(
+    {
+      url: 'https://v3.football.api-sports.io/standings?league=39&season=2018',
+      headers: {
+        'x-apisports-key': '55079badf90d509b71c69c823d5f377e',
+        'Content-Type': 'application/json'
+      }
+    },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  );
+
+});
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
