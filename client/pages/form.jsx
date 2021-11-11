8daf4a82713b5723c7a81da4937134536b149f09
Udaibir Virk <udaibirv@gmail.com>
2021-11-11 22:26:19 +0000

    created form and saved inputs using multiple values in useState hook.

--- client/pages/form.jsx
+++ client/pages/form.jsx
@@ -7,24 +7,43 @@ export const FormPractice = () => {
     phoneNumber: ''
   });
 
+  const handleSubmit = event => {
+    event.preventDefault();
+
+  };
+  const handleFirstNameInputChange = event => {
+    setValues({ ...values, firstName: event.target.value });
+  };
+
+  const handleLastNameInputChange = event => {
+    setValues({ ...values, lastName: event.target.value });
+  };
+
+  const handlePhoneNumberInputChange = event => {
+    setValues({ ...values, phoneNumber: event.target.value });
+  };
   return (
     <div className="form-container">
       <form className="register-form">
         <input
+          onChange={handleFirstNameInputChange}
           value={values.firstName}
           className="form-field"
           placeholder="First Name"
           name="firstName" />
         <input
+          onChange={handleLastNameInputChange}
           value={values.lastName}
           className="form-field"
           placeholder="Last Name"
           name="Last Name" />
         <input
+          onChange={handlePhoneNumberInputChange}
           value={values.phoneNumber}
           className="form-field"
           placeholder="Phone Number"
           name="phoneNumber" />
+        <button onClick={handleSubmit} type="button">Submit</button>
       </form>
     </div>
   );
