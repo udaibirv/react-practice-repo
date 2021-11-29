165a0de2fab64e5bff186b5523112ef1f5277dcc
udaibirv <udaibirv@gmail.com>
2021-11-29 15:58:46 -0800

    added player stats for a single fixture.

--- client/pages/teamRoster.jsx
+++ client/pages/teamRoster.jsx
@@ -2,24 +2,23 @@ import React, { useEffect, useState } from 'react';
 
 const TeamRoster = () => {
 
-  const [players, setPlayers] = useState([]);
+  const [club, setPlayers] = useState([]);
 
   const getRoster = () => {
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
-    fetch('/api/team-roster', requestOptions)
+    fetch('/api/leauge-info/england-games', requestOptions)
       .then(response => response.json())
       .then(data => {
-        const teams = data.response;
-        const teamInfo = teams.map((club, key) => {
+        const table = data.response;
+        const scorerTable = table.map((club, key) => {
           return club;
-
         });
-        console.log(teamInfo);
 
-        setPlayers(teamInfo);
+        setPlayers(scorerTable);
+
       });
   };
 
@@ -29,7 +28,30 @@ const TeamRoster = () => {
   }, [setPlayers]);
 
   return (
-    <h1>Hello</h1>
+      <div>
+      <>
+        {
+          club.map((club, key) => {
+            return (
+              <div key={key}>
+                <h1> Fixture Date: {club.fixture.date.slice(0, 10)}</h1>
+                <h1> Venue: {club.fixture.venue.name}</h1>
+                <img src={club.teams.home.logo}></img>
+                <h1> Home: {club.teams.home.name} - {club.goals.home}
+                </h1>
+                <img src={club.teams.away.logo}></img>
+                <h1> Away: {club.teams.away.name} - {club.goals.away}
+
+                </h1>
+
+              </div>
+
+            );
+          })
+        }
+      </>
+    </div>
+
   );
 
 };
