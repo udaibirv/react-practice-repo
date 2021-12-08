4a4a14d9da1bc280c99f61168b7b2280e92847e9
Udaibir Virk <udaibirv@gmail.com>
2021-12-08 23:49:07 +0000

    added player names from a specefic team.

--- client/pages/teamRoster.jsx
+++ client/pages/teamRoster.jsx
@@ -9,14 +9,14 @@ const TeamRoster = () => {
       method: 'GET',
       redirect: 'follow'
     };
-    fetch('/api/leauge-info/england-games', requestOptions)
+    fetch('/api/team-roster', requestOptions)
       .then(response => response.json())
       .then(data => {
         const table = data.response;
         const scorerTable = table.map((club, key) => {
           return club;
         });
-
+        console.log(scorerTable);
         setPlayers(scorerTable);
 
       });
@@ -33,18 +33,14 @@ const TeamRoster = () => {
         {
           club.map((club, key) => {
             return (
-              <div key={key}>
-                <h1> Fixture Date: {club.fixture.date.slice(0, 10)}</h1>
-                <h1> Venue: {club.fixture.venue.name}</h1>
-                <img src={club.teams.home.logo}></img>
-                <h1> Home: {club.teams.home.name} - {club.goals.home}
-                </h1>
-                <img src={club.teams.away.logo}></img>
-                <h1> Away: {club.teams.away.name} - {club.goals.away}
-
-                </h1>
-
+              club.players.map((player, j) => {
+                return (
+              <div key={j}>
+                <h1>{player.name}</h1>
               </div>
+
+                );
+              })
 
             );
           })
