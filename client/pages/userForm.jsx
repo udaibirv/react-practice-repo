25bb3945e9b4c3e127dad2386c3d3cefbfda2509
Udaibir Virk <udaibirv@gmail.com>
2021-12-30 00:07:38 +0000

    user-form practice

--- client/pages/userForm.jsx
+++ client/pages/userForm.jsx
@@ -1,42 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-const UserForm = () => {
-  const [users, setUsers] = useState({
+const userForm = () => {
+  const [user, setUser] = useState({
     name: '',
     password: '',
     email: ''
   });
 
-  const setName = e => {
-    setUsers({ ...users, name: e.target.value });
-    console.log(users.name);
+  const handleName = e => {
+    setUser({ ...user, name: e.target.value });
   };
 
-  const setPassword = e => {
-    setUsers({ ...users, password: e.target.value });
-    console.log(users.password);
+  const handlePassword = e => {
+    setUser({ ...user, password: e.target.value });
   };
 
-  const setEmail = e => {
-    setUsers({ ...users, email: e.target.value });
-    console.log(users.email);
+  const handleEmail = e => {
+    setUser({ ...user, email: e.target.value });
   };
 
   return (
-    <div className='container container-fluid'>
-      <h1>Hello, Please fill out this form</h1>
-      <div className="form">
-        <form>
-          <label htmlFor='name'>Name</label>
-          <input type='text' className="name" name='name' value={users.name} onChange={setName} />
-          <label htmlFor='password'>Password</label>
-          <input type='text' className='password' name='password' value={users.password} onChange={setPassword} />
-          <label htmlFor='name'>Email</label>
-          <input type='email' className='email' name='email' value={users.email} onChange={setEmail} />
-        </form>
-      </div>
+  <div>
+      <h1>Hello, Please complete this form</h1>
+      <div className="form user-form">
+        <label htmlFor='name'>Name</label>
+        <input type='text' value={user.name} onChange={handleName} className='input' />
+        <label htmlFor='password'>Password</label>
+        <input type='password' value={user.password} onChange={handlePassword} className='input' />
+        <label htmlFor='email'>Password</label>
+        <input type='email' value={user.email} onChange={handleEmail} className='input' />
     </div>
+  </div>
   );
 };
 
-export default UserForm;
+export default userForm;
